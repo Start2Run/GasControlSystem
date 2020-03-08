49869cd49615faa675ba6b7721c9d6054f52c68b
Start2Run <mihaivs@yahoo.com>
2020-03-08 00:05:51 -0400

    Add PID controller

--- src/lib/pid-controller.h
+++ src/lib/pid-controller.h
@@ -1,14 +1,25 @@
-#ifndef PID_H_ 
-#define PID_H_ #include 
+#ifndef __PID_CONTROLLER__
+#define __PID_CONTROLLER__ 
+
 //Define parameter 
 #define epsilon 0.01 
-#define dt 0.01             
-//100ms loop time 
-#define MAX  100                   
-//For Current Saturation 
-#define MIN 0
-#define Kp  0.1 #define Kd  0.01 #define Ki  0.005
+#define dt 0.01 //100ms loop time     
 
-float PIDcal(float setpoint,float actual_position);
+#define MAX 50 //For Current Saturation 
+#define MIN -50
+#define Kp  0.1 
+#define Kd  0.01 
+#define Ki  0.005
+
+extern struct PID
+{
+    float error;
+    float integral;
+    float output;
+};
+
+void pid_init(struct PID *pid);
+
+void pid_cal(float setpoint,float actual_value, struct PID *pid);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -9,7 +9,8 @@
 #include <semaphore.h> 
 #include "./lib/tcp-client.h"
 #include "./lib/state-helper.h"
- 
+#include "./lib/pid-controller.h"
+
 int main(void)
 {
     // printf("Enter the sever IP address: "); 
@@ -22,7 +23,6 @@ int main(void)
 	// {
 	// 	return -1;
 	// }
-
 		
 	struct ControlModel model1 = GetGazControlState(30);
 	struct ControlModel model2 = GetGazControlState(60);
@@ -30,5 +30,16 @@ int main(void)
 
 	Ventilation vent = GetGlobalVentilation(model1,model2,model3);
 	Aeration aer = GetGlobalAertation(model1,model2,model3);
+
+	struct PID pid;
+
+	pid_init(&pid);
+
+	double val = 20;
+    for (int i = 0; i < 100; i++) {
+		pid_cal(-40,val,&pid);
+        printf("val:% 7.3f inc:% 7.3f\n", val, pid.output);
+        val += pid.output;
+    }
     return 0;
 }
